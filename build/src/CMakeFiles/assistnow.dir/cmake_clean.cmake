342e3eaca48e60c441e08a59c2587b6aa9b225af
uched41 <uched41@yahoo.com>
2019-09-06 11:28:34 +0300

    first commit

---
+++ build/src/CMakeFiles/assistnow.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assistnow.dir/main.cpp.o"
+  "assistnow.pdb"
+  "assistnow"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/assistnow.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include "stdio.h"
+
+int main(void){
+	printf("Hello my man \n");
+	return 0;
+}
